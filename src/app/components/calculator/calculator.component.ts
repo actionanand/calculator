c6c67d84cb25217fddc4f0c760593994b3d6a1e2
Anand raja <meetanandns@gmail.com>
2022-05-02 12:13:42 +0000

    cal logic change cont

--- src/app/components/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator.component.ts
@@ -9,7 +9,17 @@ export class CalculatorComponent implements OnInit {
 
   buttonVals = ['1','2','3','/','4','5','6','X','7','8','9','-','(',')','0','+','c','.','=','%'];
 
+  operands = ['X', '/', '+', '%', '-', '='];
+  parentheses = ['(', ')'];
+  actionBtns = ['c']
+
   inputVals = '';
+  prevOperand = '';
+  curOperand = '';
+  prevParenthhesis = '';
+  curParenthesis = '';
+  prevValue = '';
+  curValue = '';
 
   constructor() { }
 
@@ -18,21 +28,71 @@ export class CalculatorComponent implements OnInit {
 
   getInput(e:any, val: string) {
     e.preventDefault();
-    if(val == 'X') {
-      this.inputVals += "*";
+
+    if(this.operands.indexOf(val) !== -1) {
+      val == 'X' ? this.curOperand = '*' : this.curOperand = val;
+      this.operandLogic();
+      console.log('Operand ', val);
+    } else if (this.parentheses.indexOf(val) !== -1) {
+      this.curParenthesis = val;
+      this.parenthesesLogic();
+      console.log('parentheses Button ', val);
+    } else {
+      this.curValue = val;
+      this.valueLogic();
+      console.log('Other ', val);
     }
-    else if(val == "=" && this.inputVals == '.') {
-      alert("Please enter a valid mathematical expression.");
+
+    // if(val == 'X') {
+    //   this.inputVals += "*";
+    // }
+    // else if(val == "=" && this.inputVals == '.') {
+    //   alert("Please enter a valid mathematical expression.");
+    // }
+    // else if(val == "=") {
+    //   this.inputVals = eval(this.inputVals);
+    // }
+    // else if(val == "c") {
+    //   this.inputVals = this.inputVals.slice(0,this.inputVals.length-1);
+    // }
+    // else {
+    //   this.inputVals += val;
+    // }
+  }
+
+  operandLogic() {
+    if (this.prevOperand == this.curOperand) {
+      console.log('Same operand')
+      return;
     }
-    else if(val == "=") {
-      this.inputVals = eval(this.inputVals);
+    console.log('diff operand')
+    this.inputVals += this.curOperand;
+    this.prevOperand = this.curOperand;
+    this.prevParenthhesis = '';
+    this.prevValue = '';
+  }
+
+  parenthesesLogic() {
+    if (this.curParenthesis == this.prevParenthhesis) {
+      console.log('Same parenthesis')
+      return;
     }
-    else if(val == "c") {
-      this.inputVals = this.inputVals.slice(0,this.inputVals.length-1);
+    console.log('diff parenthes')
+    this.inputVals += this.curParenthesis;
+    this.prevParenthhesis = this.curParenthesis;
+    this.prevOperand = '';
+    this.prevValue = '';
+  }
+
+  valueLogic() {
+    if(this.prevValue == this.curValue && this.prevValue == '.') {
+      console.log('again dot')
+      return;
     }
-    else {
-      this.inputVals += val;
-    }
+    this.inputVals += this.curValue;
+    this.prevValue = this.curValue;
+    this.prevOperand = '';
+    this.prevParenthhesis = '';
   }
 
 }
