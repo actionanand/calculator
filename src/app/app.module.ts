a85ac944a391352fd51b7732bd65bd143a9ba94b
Anand raja <meetanandns@gmail.com>
2022-05-02 07:44:26 +0000

    infinite delete logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BubblingTextDirective } from './directives/bubbling-text.directive';
 import { VantaComponent } from './components/vanta/vanta.component';
+import { TypeDeleteComponent } from './components/type-delete/type-delete.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BubblingTextDirective,
-    VantaComponent
+    VantaComponent,
+    TypeDeleteComponent
   ],
   imports: [
     BrowserModule
--- src/app/components/vanta/vanta.component.ts
+++ src/app/components/vanta/vanta.component.ts
@@ -7,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VantaComponent implements OnInit {
 
+  todaySdate = "Today's date is " + new Date().toUTCString().slice(5, 16) + '.';
+  todayStime = 'And local time is ' + new Date().toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true }) + '.';
+
   textArray = ['Anand', 'Raja', 'Programming', 'is', 'an', 'art'];
+
+  typeDeleteArray = [
+    'Hello viewers...',
+    'You can write anything for display...',
+    this.todaySdate,
+    this.todayStime,
+    'Programming is an art 💚'
+  ];
 
   constructor() { }
 
